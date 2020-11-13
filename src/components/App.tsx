7b5e288e09fcb78ec4af23b002376fa2661a8564
georgevernie <jonlangner@gmail.com>
2020-11-13 11:17:33 -0600

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Initial Commit</h1>
+    </div>
+  );
+}
+
+export default App;
